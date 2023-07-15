6edcf7cc587d66c423c8c95cb9fa4321394ff405
Dasha <dashasegoleva2428@gmail.com>
2023-07-15 16:45:46 +0300

    site

--- advertisments/advertisments/settings.py
+++ advertisments/advertisments/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'advertisments.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
